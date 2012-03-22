4e6d87e9b56317b7dcc344ff7f2ac297432b444e
Rich Liebling <rliebling@gmail.com>
2012-03-22 10:27:26 -0700

    initial commit after rails new ember-rails -d mysql
    no changes to what was auto-generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberRails::Application.config.secret_token = 'cee232f5a058c663afc12ca8dfe5b73dd37a60a7a5e05534fab7aa6b7614e262ca7f1b6fe5490ce04f1df66c540148d1024f3e3fb5c11c7535dbd1974a16f48c'
