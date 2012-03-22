76edd19ab0b3847cfaf9099e2432031c389c2b35
Rich Liebling <rliebling@gmail.com>
2012-03-22 15:44:41 -0700

    nav view with binding
    Notes:
    * if we try to attach the navView inside the init() for navController, then
    we cannot bind the view to the controller.  The problem is we want to set
    the binding using some globally accessible path, eg Example.navController.content
    But, inside the init() for navController we still have not assigned to
    Example.navController.  Attempting this yields an exception "Must use Ember.set to access this property"
    So, we moved the attachNavView() method call to be done explictly in Example#ready()
    
    * we created some added complexity in NavController using a closure to define
    addRandomElement in order to make it accessible in a setTimeout.  Could probably
    have used some jquery mechanism to setTimeout with a bound version of a instance
    method on this class (binding this to the navController instance, instead of
    the window).  Our point was just to show that we can add to content and
    the view auto-updates (via the binding).

--- app/assets/javascripts/example/nav_controller.js
+++ app/assets/javascripts/example/nav_controller.js
@@ -1,14 +1,33 @@
 Example.NavController = Em.Object.extend({
+  content: [],
   init: function() {
     this._super();
 
+
     // automatically called when instance is created
     console.log("NavController.init");
-    this.attachNavView();
+
+    var myThis = this;
+    addRandomElement =  function() {
+      myThis.content.pushObject(Ember.Object.create({int: myThis.randomInt(10), alpha: myThis.randomAlpha()}));
+    };
+    for(var i=0;i<10;i++){
+      addRandomElement();
+    }
+    setTimeout(addRandomElement, 1000);
   },
 
   attachNavView: function() {
-    this.navView = Example.NavView.create({}).replaceIn($('#nav'));
+    this.navView = Example.NavView.create({
+      linksBinding: 'Example.navController.content'
+    }).replaceIn($('#nav'));
+  },
+
+  randomInt: function(max) {
+    return Math.floor((max * Math.random())) + 1;
+  },
+  randomAlpha: function() {
+    return String.fromCharCode('A'.charCodeAt() + this.randomInt(26) - 1);
   }
 
 });
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,6 +3,7 @@ Example = Ember.Application.create({
   ready: function() {
     console.log("starting ready()");
     this.navController = this.NavController.create({});
+    this.navController.attachNavView(); // called after we assign Example.navController, else binding of navView to the controller fails
     console.log("ending ready()");
   }
 });
